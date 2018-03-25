5a94615d91b8d49d65294ac8a707ac5f208e18e5
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-03-25 15:44:49 +0100

    make get request to coin api

--- src/server.js
+++ src/server.js
@@ -8,6 +8,19 @@ const port = process.env.PORT || 3003
 const server = http.createServer(router)
 
 // TODO: make GET request to populate data.json
+const url = 'https://coinbin.org/coins'
+
+get(url, res => {
+
+  let json = ''
+  res.once('data', chunk => {
+    json += chunk
+  })
+
+  res.on('end', () => {
+    log(json)
+  })
+})
 
 server.listen(port, err => {
   if (err) return error('Something went wrong ', err)
