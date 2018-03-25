f58d3420a4c80da8a9024496f45e3937f1c90d13
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-03-25 18:37:09 +0100

    implements filterApiData

--- test/toolkit.test.js
+++ test/toolkit.test.js
@@ -9,9 +9,28 @@ test('Tape is working', assert => {
 
 test('Testing formatApiData', assert => {
   // test returns and object
+  const formattedDataObj = formatApiData(fakeApiResObj)
   assert.equal(
-    typeof formatApiData(fakeApiResObj),
+    typeof formattedDataObj,
     'object',
     'formatApiData should return an object')
+
+  // test each key has an object
+  if (Object.keys(formattedDataObj).length > 0) {
+    assert.pass('formatted data is populated')
+  } else {
+    assert.fail('formatted data is not populated')
+  }
+
+  // test each object has two keys
+  const coins = Object.keys(formattedDataObj)
+  const sumOfProps = coins.reduce((sum, coin) => {
+    return sum + Object.keys(formattedDataObj[coin]).length
+  }, 0)
+  const avgNumOfProps = Math.ceil(sumOfProps / coins.length)
+  assert.equals(avgNumOfProps, 2, 'each inner object should have two properties')
   assert.end()
+
+  // const actual
+  // const expected
 })
