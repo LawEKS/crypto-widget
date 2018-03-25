f967c88681d391628cf29b40a2d29e6c15c7c02a
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-03-25 15:28:23 +0100

    implement serving home page with static files

--- src/handler.js
+++ src/handler.js
@@ -1,23 +1,67 @@
 const fs = require('fs')
 const qs = require('querystring')
+const path = require('path')
+const { resServerError, resResourceError, resFile200 } = require('./toolkit')
 
 const { log } = console
 
 const handleHome = (req, res) => {
   log('-handle- home')
+  const filePath = path.join(__dirname, '..', 'public', 'index.html')
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      resServerError(res)
+    } else {
+      resFile200(res, file)
+    }
+  })
 }
+
+const handleIndex = (req, res) => {
+  const filePath = path.join(__dirname, '..', 'public', 'index.html')
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      resServerError(res)
+    } else {
+      resFile200(res, file)
+    }
+  })
+}
+
 const handleStatic = (req, res) => {
   log('-handle- static')
+
+  const ext = req.url.split('.')[1]
+  const contentType = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/javascript',
+    ico: 'image/x-icon'
+  }[ext]
+  log('-content type- ' + contentType)
+
+  const filePath = path.join(__dirname, '..', req.url)
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      resServerError(res)
+    } else {
+      resFile200(res, file, contentType)
+    }
+  })
 }
+
 const handleSuggestions = (req, res) => {
   log('-handle- suggestions')
 }
+
 const handlePageNotFound = res => {
   log('-handle- page not found')
+  resResourceError(res)
 }
 
 module.exports = {
   handleHome,
+  handleIndex,
   handleStatic,
   handleSuggestions,
   handlePageNotFound
