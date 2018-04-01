a3b44511492ef48479c6d02cb1ebb87b71c40eaa
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-04-01 21:23:48 +0100

    linting

--- public/js/request.js
+++ public/js/request.js
@@ -1,25 +1,26 @@
+/* eslint-disable */
 function getSuggestions(subString) {
-  var url = '/suggestions&search=' + subString
-  var xhr = new XMLHttpRequest()
+  var url = '/suggestions&search=' + subString;
+  var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
-      var resObj = JSON.parse(this.responseText)
-      renderSuggestion(resObj)
+      var resObj = JSON.parse(this.responseText);
+      renderSuggestion(resObj);
     }
-  }
-  xhr.open('GET', url, true)
-  xhr.send()
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
 }
 
 function getOverview(idString) {
-  var url = '/forecast&ticker=' + idString
-  var xhr = new XMLHttpRequest()
+  var url = '/forecast&ticker=' + idString;
+  var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
-      var resObj = JSON.parse(this.responseText)
-      renderOverview(resObj)
+      var resObj = JSON.parse(this.responseText);
+      renderOverview(resObj);
     }
-  }
-  xhr.open('GET', url, true)
-  xhr.send()
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
 }
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,26 +1,27 @@
+/* eslint-disable */
 function listenUserSearchInput() {
-  console.log('listening for user input')
+  console.log('listening for user input');
 
-  var input = document.querySelector('.search')
+  var input = document.querySelector('.search');
   input.addEventListener('keyup', function(e) {
-    var userInput = e.target.value.trim()
-    getSuggestions(userInput, renderSuggestion)
-  })
+    var userInput = e.target.value.trim();
+    getSuggestions(userInput, renderSuggestion);
+  });
 }
 
 function listenUserSelectItem() {
-  var listContainer = document.querySelector('.list-container')
+  var listContainer = document.querySelector('.list-container');
   listContainer.addEventListener('click', function(e) {
-    var text = e.target.textContent
-    var input = document.querySelector('.search')
-    input.value = text
-    var item = e.target
-    if (item.nodeName === 'UL' || item.nodeName === 'SECTION') return
-    var ticker = item.dataset.ticker
-    console.log(ticker)
-    getOverview(ticker)
-  })
+    var text = e.target.textContent;
+    var input = document.querySelector('.search');
+    input.value = text;
+    var item = e.target;
+    if (item.nodeName === 'UL' || item.nodeName === 'SECTION') return;
+    var ticker = item.dataset.ticker;
+    console.log(ticker);
+    getOverview(ticker);
+  });
 }
 
-listenUserSearchInput()
-listenUserSelectItem()
+listenUserSearchInput();
+listenUserSelectItem();
